2955d8c53b6d78115e70fcb36d3d57cee5b6e9d3
FernandoRRT <suporteiros@gmail.com>
2021-12-14 16:19:04 -0300

    Falta somente o tratamento do form

--- scripts.js
+++ scripts.js
@@ -1,15 +1,45 @@
-function func1(valor1, valor2) {
-    return valor1 + valor2;
-}
+document.getElementById("btnContactUs").addEventListener("click", envia);
 
-console.log('Return normal '+func1(1, 2));
+function envia () {
 
-function func2(valor1) {
-    return function (valor2) {
-        return valor1 + valor2;
+    let nome = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let subject = document.getElementById("subject").value;
+    let message = document.getElementById("message").value;
+
+    if (nome) {
+        if (email) {
+            var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+                if (reg.test(email)){
+                    if (subject != "default") {
+                        if (message) {
+                            alert("Certinho");
+
+                            //https://github.com/ajudadoprogramador/php-formulario-html-com-ajax
+                        } else {
+                            alert("Por favor, explique melhor em quê posso te ajudar.");
+                            message.focus;
+                        }
+                    } 
+                    else {
+                        alert("Por favor, selecione o assunto da mensagem.");
+                        subject.focus;
+                    }
+                }
+                else{
+                alert("Preencha o email corretamente no formato EMAIL@PROVEDOR.xxx");
+                email.focus;
+                }
+            }
+        else {
+            alert("Por favor, preencha o campo do email.");
+            email.focus;    
+        }
     }
+    else {
+        alert("Por favor, preencha o campo do nome.");
+        nome.focus;
+
+    }
+    
 }
-
-const func2valor1 = func2(10);
-
-console.log('Returna alterado ' + func2valor1(20));
