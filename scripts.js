2d600c6a506e211d7579f62a90bc80e1f5dd5702
FernandoRRT <suporteiros@gmail.com>
2021-12-15 11:42:57 -0300

    Problema de validação de formulario

--- scripts.js
+++ scripts.js
@@ -1,39 +1,126 @@
 
 
-$('#btnContactUs').click(function(){
+// $('#btnContactUs').click(function(e){
+//     e.preventDefault();
 
-    if ($('#subject').val() == "default") {
-        alert("Por favor, selecione o assunto da mensagem.");
-        $('#subject').focus;
-    }
-    else {
-        if($('#btnContactUs').val() == 'Enviando...'){
-			return(false);
-		}
-		
-		$('#btnContactUs').val('Enviando...');
-		
-		$.ajax({
-			url: 'valida-formulario.php',
-			type: 'post',
-			dataType: 'html',
-			data: {
-				'nome': $('#name').val(),
-				'email': $('#email').val(),
-				'assunto': $('#subject').val(),
-				'Mensagem': $('#message').val(),
-			}
-		}).done(function(data){
+//     if ($('#subject').val() == "default") {
+//         alert("Por favor, selecione o assunto da mensagem.");
+//         $('#subject').focus;
+//     }
+//     else {
+//         $.ajax({
+//             method: "POST",
+//             url: "valida-formulario.php",
+//             data: {
+//                     'nome': $('#name').val(),
+//                     'email': $('#email').val(),
+//                     'assunto': $('#subject').val(),
+//                     'mensagem': $('#message').val(),
+//                 },
+//                 beforeSend : function(){
+//                      $("#resultado").html("ENVIANDO...");
+//                 }
+//            })
+//            .done(function(msg){
+//                 $("#resultado").html(msg);
+//            })
+//            .fail(function(jqXHR, textStatus, msg){
+//                 alert(msg);
+//            });
 
-            //alert(data);
-			$('#btnContactUs').val('Enviar mensagem');
-			$('#name').val('');
-			$('#email').val('');
-			$('#subject').val('');
-			$('message').val('');
+
+
+//         // if($('#btnContactUs').val() == 'Enviando...'){
+// 		// 	return(false);
+// 		// }
+		
+// 		// $('#btnContactUs').val('Enviando...');
+		
+// 		// $.ajax({
+// 		// 	url: 'valida-formulario.php',
+// 		// 	type: 'post',
+// 		// 	dataType: 'html',
+// 		// 	data: {
+// 		// 		'nome': $('#name').val(),
+// 		// 		'email': $('#email').val(),
+// 		// 		'assunto': $('#subject').val(),
+// 		// 		'Mensagem': $('#message').val(),
+// 		// 	}
+// 		// }).done(function(data){
+
+//         //     //alert(data);
+// 		// 	$('#btnContactUs').val('Enviar mensagem');
+// 		// 	$('#name').val('');
+// 		// 	$('#email').val('');
+// 		// 	$('#subject').val('');
+// 		// 	$('message').val('');
 			
-		});
-    }
+// 		// });
+//     }
 
     
+// });
+
+
+$(function(){
+    $("#formulario").on("submit", function(e){
+              
+        // Cancelamos el evento si se requiere 
+        e.preventDefault();
+ 
+        // Obtenemos los datos del formulario 
+        
+        //let formData = new FormData(document.getElementById("formulario"));
+        //formData.append("dados", "valores");
+               
+       //ssss console.log(formData);
+       console.log($('#name').val());
+       console.log($('#email').val());
+       console.log($('#phone').val());
+       console.log($('#message').val());
+
+
+        // Enviamos los datos al archivo PHP que procesará el envio de los datos a un determinado correo 
+        $.ajax({
+            url: "valida-formulario.php",
+            type: "POST",
+            dataType: "json",
+            //data: formData,
+            data: {
+              name: $('#name').val(),
+              email: $('#email').val(),
+              phone: $('#phone').val(),
+              message: $('#message').val(),
+              },
+            cache: false,
+            contentType: false,
+            processData: false,
+            beforeSend: function() {
+              $('#formulario').html("Enviando mensagem...");
+            },
+        })
+ 
+        // Cuando el formulario es enviado, mostramos un mensaje en la vista HTML 
+        // En el archivo enviarcorreo.php devuelvo el valor '1' el cual es procesado con jQuery Ajax 
+        // y significa que el mensaje se envio satisfactoriamente. 
+        .done(function (res) {                  
+ 
+          if(res.a == "1"){
+                    
+            // Mostramos el mensaje 'Tu Mensaje ha sido enviado Correctamente !' 
+            $("#formulario").html(res.b);                   
+            $("#formulario").trigger("reset");    
+ 
+          }  else {                                       
+            $("#formulario").html(res.b); 
+          }
+                                                      
+        })
+ 
+        // Mensaje de error al enviar el formulario 
+        .fail(function (res) {                    
+            $("#formulario").html(res.b);
+        });
+ 
+    });
 });
--- valida-formulario.php
+++ valida-formulario.php
@@ -1,44 +1,51 @@
 <?php
-	if(strcasecmp('formulario', $_POST['metodo']) == 0){
 
-		$html = 'Nome: '.$_POST['nome'];
-		$html .= "\n";
-		$html .= 'Email: '.$_POST['email'];
-		$html .= "\n\n Obrigado pela mensagem.";
-		echo $html;
+//if(!$_POST) exit;
+ 
+// 		$html = 'Nome: '.$_POST['nome'];
+// 		$html .= "\n";
+// 		$html .= 'Email: '.$_POST['email'];
+// 		$html .= "\n\n Obrigado pela mensagem.";
+// 		echo $html;
+if ($_POST) {
+	$nome = $_POST['name'];
+	$telefone = $_POST['phone'];
+	$email = $_POST['email'];
+	$assunto = 'Mensagem nova do site!';
+	//$mensagem = $_POST['message'];
+	$destino = "suporteiros@gmail.com";
 
-
-$para = "suporteiros@gmail.com";
-$nome = $_POST['name'];
-$assunto = $_POST['subject'];
-$email = $_POST['email'];
-
-
- //4 – Agora definimos a  mensagem que vai ser enviado no e-mail
-$mensagem = "<strong>Nome:  </strong>".$nome;
-$mensagem .= "<br>  <strong>Mensagem: </strong>".$_POST['message'];
-$mensagem .= '<h1 style="text-align: center;"><span style="font-family:verdana,geneva,sans-serif">Tem mensagem nova pra voc&ecirc;!</span></h1>';
-$mensagem .= '<p><span style="font-family:georgia,serif">Rog&eacute;rio, <strong>'.$nome;
-$mensagem .= '</strong>deseja falar contigo:</span></p>';
-$mensagem .= '<p><span style="font-family:tahoma,geneva,sans-serif">'.$_POST['message'];
-$mensagem .= '</span></p><hr /><address>Esse email foi enviado atrav&eacute;s do seu site.</address>';
-$mensagem .= '<div>Nome: '.$nome;
-$mensagem .= '</div><div>Email: '.$email;
-$mensagem .= '</div><address>&nbsp;</address>';
+	//4 – Agora definimos a  mensagem que vai ser enviado no e-mail
+	$mensagem = '<h1 style="text-align: center;"><span style="font-family:verdana,geneva,sans-serif">Tem mensagem nova pra voc&ecirc;!</span></h1>';
+	$mensagem .= '<p><span style="font-family:georgia,serif">Rog&eacute;rio, <strong>'.$_POST['name'].'</strong>deseja falar contigo:</span></p>';
+	$mensagem .= '<p><span style="font-family:tahoma,geneva,sans-serif">'.$_POST['message'].'</span></p><hr />';
+	$mensagem .= '<address>Esse email foi enviado atrav&eacute;s do seu site.</address>';
+	$mensagem .= '<div>Nome: '.$_POST['name'].'</div><div>Email: '.$_POST['email'].'</div><div>Telefone: '.$_POST['phone'].'</div><address>&nbsp;</address>';
 
 
-//codificações corretas e  tudo mais.
-$headers =  "Content-Type:text/html; charset=UTF-8\n";
-$headers .= "From:  dominio.com.br<sistema@dominio.com.br>\n";
-//Vai ser //mostrado que  o email partiu deste email e seguido do nome
-$headers .= "X-Sender:  <sistema@dominio.com.br>\n";
-//email do servidor //que enviou
-$headers .= "X-Mailer: PHP  v".phpversion()."\n";
-$headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
-$headers .= "Return-Path:  <".$email.">\n";
-//caso a msg //seja respondida vai para  este email.
-$headers .= "MIME-Version: 1.0\n";
-
-mail($para, $assunto, $mensagem, $headers);
+	//codificações corretas e  tudo mais.
+	$headers =  "Content-Type:text/html; charset=UTF-8\n";
+	$headers .= "From:  profrogerio.com.br<sistema@profrogerio.com.br>\n";
+	//Vai ser //mostrado que  o email partiu deste email e seguido do nome
+	$headers .= "X-Sender:  <sistema@dominio.com.br>\n";
+	//email do servidor //que enviou
+	$headers .= "X-Mailer: PHP  v".phpversion()."\n";
+	$headers .= "X-IP:  ".$_SERVER['REMOTE_ADDR']."\n";
+	$headers .= "Return-Path:  <".$email.">\n";
+	//caso a msg //seja respondida vai para  este email.
+	$headers .= "MIME-Version: 1.0\n";
+	
+	if(mail($destino, $assunto, $mensagem, $headers)) {
+		// Si el correo es enviado correctamente, mostramos un mensaje 
+		$a = 1;
+		$b = "<div class='alert alert-success'>Tu Mensaje ha sido enviado Correctamente !</div>";
+	$dab = array(
+		"a" => $a, 
+		"b" => $b
+	);
+	echo (json_encode($dab));
+	} else {
+		echo 'ERROR!';
 	}
+}
 ?>
