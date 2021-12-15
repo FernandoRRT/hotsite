2841c17531b313928df142d8b132a72e7666004d
Fernando <suporteiros@gmail.com>
2021-12-14 23:34:26 -0300

    Corrigindo o PHP e Ajax

--- scripts.js
+++ scripts.js
@@ -1,45 +1,39 @@
-document.getElementById("btnContactUs").addEventListener("click", envia);
 
-function envia () {
 
-    let nome = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let subject = document.getElementById("subject").value;
-    let message = document.getElementById("message").value;
+$('#btnContactUs').click(function(){
 
-    if (nome) {
-        if (email) {
-            var reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-                if (reg.test(email)){
-                    if (subject != "default") {
-                        if (message) {
-                            alert("Certinho");
-
-                            //https://github.com/ajudadoprogramador/php-formulario-html-com-ajax
-                        } else {
-                            alert("Por favor, explique melhor em quê posso te ajudar.");
-                            message.focus;
-                        }
-                    } 
-                    else {
-                        alert("Por favor, selecione o assunto da mensagem.");
-                        subject.focus;
-                    }
-                }
-                else{
-                alert("Preencha o email corretamente no formato EMAIL@PROVEDOR.xxx");
-                email.focus;
-                }
-            }
-        else {
-            alert("Por favor, preencha o campo do email.");
-            email.focus;    
-        }
+    if ($('#subject').val() == "default") {
+        alert("Por favor, selecione o assunto da mensagem.");
+        $('#subject').focus;
     }
     else {
-        alert("Por favor, preencha o campo do nome.");
-        nome.focus;
+        if($('#btnContactUs').val() == 'Enviando...'){
+			return(false);
+		}
+		
+		$('#btnContactUs').val('Enviando...');
+		
+		$.ajax({
+			url: 'valida-formulario.php',
+			type: 'post',
+			dataType: 'html',
+			data: {
+				'nome': $('#name').val(),
+				'email': $('#email').val(),
+				'assunto': $('#subject').val(),
+				'Mensagem': $('#message').val(),
+			}
+		}).done(function(data){
 
+            //alert(data);
+			$('#btnContactUs').val('Enviar mensagem');
+			$('#name').val('');
+			$('#email').val('');
+			$('#subject').val('');
+			$('message').val('');
+			
+		});
     }
+
     
-}
+});
